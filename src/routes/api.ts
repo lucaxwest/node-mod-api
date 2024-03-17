3276050cdb6dccb9f13e8539fb46d72586d7c9f9
lucaxwest <lukinhas44_santos@hotmail.com>
2024-03-16 23:58:26 -0300

    add on github

---
+++ src/routes/api.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import * as ApiController from "../controllers/apiController";
+
+const router = Router();
+
+router.get('/ping', ApiController.ping);
+router.get('/random', ApiController.random)
+router.get('/nome/:nome', ApiController.nome)
+
+
+router.post('/frases', ApiController.createPhrase)
+router.get('/frases', ApiController.listPhrases)
+router.get('/frases/aleatoria', ApiController.randomPhrase)
+router.get('/frases/:id', ApiController.getPhrase)
+router.put('/frases/:id', ApiController.updatePhrase)
+router.delete('/frases/:id', ApiController.deletePhrase)
+
+
+
+export default router;
